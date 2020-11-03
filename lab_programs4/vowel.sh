87eaea054c5618347ba62f4e1bfecb86ed5794b8
Swathivcs19 <55060858+Swathivcs19@users.noreply.github.com>
2020-11-03 05:03:23 -0500

    Add files via upload

---
+++ lab_programs4/vowel.sh
@@ -0,0 +1,11 @@
+echo "enter the character"
+read char
+
+case $char in
+	a)echo "The given character is Vowel";;
+	e)echo "The given character is Vowel";;
+	i)echo "The given character is Vowel";;
+	o)echo "The given character is Vowel";;
+	u)echo "The given character is Vowel";;
+	*)echo "The given character is Consonant"
+esac
